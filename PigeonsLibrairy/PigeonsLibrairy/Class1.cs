1e10c64e0bf9c0ebef98d19e8ad6da9df04fe486
Marc-Éric Boury <marc-eric.boury.1@collegeahuntsic.qc.ca>
2016-03-16 17:52:33 -0400

    Test 2 no touching master

--- PigeonsLibrairy/PigeonsLibrairy/Class1.cs
+++ PigeonsLibrairy/PigeonsLibrairy/Class1.cs
@@ -9,6 +9,7 @@ namespace PigeonsLibrairy
     public class Class1
     {
         // Bruno pushing test on master
-        // MarcEric Pushing test on MarcEric branch
+        // MarcEric Pushing test on MarcEric branch (failed: modified local "master" branch, so had to push to master too (couldnt switch branch)
+        // Test 2 MarcEric, pushing from own local branch (locl MarcEric) to own foreign branch, no touching master.
     }
 }
