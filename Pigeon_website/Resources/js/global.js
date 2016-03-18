27110c581c4090fa9540edb97046df61e8901bb3
Laurent Boisvert <laurent.boisvert.1@collegeahuntsic.qc.ca>
2016-03-18 10:55:51 -0400

    modifs modal js (modal bind sur escape), connect form dans modal

--- Pigeon_website/Resources/js/global.js
+++ Pigeon_website/Resources/js/global.js
@@ -31,3 +31,14 @@ window.onload = function () {
     $('#myModal').modal();
 };
 
+// bind escape key to close modal
+
+var ESCAPE_KEY = 27;
+$(document).keyup(function (e) {
+
+    if (e.keyCode === ESCAPE_KEY) {
+        $(".modal").modal('hide');
+    }
+        
+
+});
