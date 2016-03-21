af4a4c49c0c71361f6cafd07149cb2f789b2dea1
Laurent Boisvert <laurent.boisvert.1@collegeahuntsic.qc.ca>
2016-03-21 11:32:04 -0400

    entity fixation

--- Pigeon_website/Index.aspx.cs
+++ Pigeon_website/Index.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PigeonsLibrairy.Controller;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,20 @@ public partial class Index : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+   
+    }
+    protected void connectButton_Click(object sender, EventArgs e)
+    {
+        string userEmail = this.userEmail.Text;
+        string userPassword = this.userPassword.Text;
 
+        Controller controller = new Controller();
+        List<PigeonsLibrairy.Model.person> lp = controller.PersonService.GetPersonsBy("email", userEmail).ToList();
+        if (lp[0] != null)
+        {
+
+            // todo: vérif. passowrd
+            Response.Redirect("http://google.com");
+        }
     }
 }
