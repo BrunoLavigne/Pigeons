efa7ecf4f68e3721ab57f9c415caa6ffc5d44652
laurentboisvert <laurent.boisvert.1@collegeahuntsic.qc.ca>
2016-03-25 14:11:52 -0400

    anim changes

--- Pigeon_website/Resources/js/global.js
+++ Pigeon_website/Resources/js/global.js
@@ -9,20 +9,32 @@ window.onload = function () {
 
     var t1 = new TimelineLite();
 
+    // Make pigeon bigger and smaller
     var pigeonSwell = TweenMax.to('.Home-logo-container', 5, {
         scale: 0.85,
-        //skewY: 4,
-        //skewX: 4,
         repeat: -1,
         yoyo: true,
         ease: Power1.easeInOut,
         onComplete: function () {
-            console.log('done');
+            console.log("... anim: done with PIGEON-SWELL");
         }
     });
 
+    // Rotate the pigeon on Y axis
+    var pigeonRotate = TweenMax.to('.Home-logo-container', 1.5, {
+        skewY: 180,
+        repeat: -1,
+        repeatDelay: 10,
+        yoyo: true,
+        ease: Back.easeInOut.config(1.4),
+        onComplete: function() {
+            console.log("... anim: done with PIGEON-ROTATE");
+        }
+    });
 
+    // Add anims to timeline
     t1.add(pigeonSwell, 1);
+    t1.add(pigeonRotate, 5);
 };
 
 
