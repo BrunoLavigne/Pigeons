d58753e0dc4a9a6c103d873d5d53ce287bca0a8e
franz <kezfranz@yahoo.fr>
2016-04-01 10:31:43 -0400

    klk changement

--- PigeonsLibrairy/UnitTesting/UnitTest1.cs
+++ PigeonsLibrairy/UnitTesting/UnitTest1.cs
@@ -1,22 +1,32 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PigeonsLibrairy.DAO.Implementation;
-using PigeonsLibrairy.Service.Implementation;
 using PigeonsLibrairy.Model;
-using System.Data.Entity;
+using PigeonsLibrairy.DAO.Implementation;
 
 namespace UnitTesting
 {
     [TestClass]
     public class UnitTest1
     {
+        pigeonsEntities1 context = new pigeonsEntities1();
+        person personToTest = new person();
         [TestMethod]
         public void InsertTest()
         {
-            pigeonsEntities1 context = new pigeonsEntities1();
-            PersonService service = new PersonService(context);
-            //service.
-            //personne.Insert()
+            personToTest.Name = "Toto";
+            personToTest.Email = "toto@gmail.com";
+            personToTest.Profile_picture_link = "http://toto.com";
+            personToTest.Inscription_date = DateTime.Now;
+            personToTest.Birth_date = DateTime.Parse("1980-01-15");
+            personToTest.Phone_number = "5141234567";
+            personToTest.Organization = "Toto Inc";
+            personToTest.Position = "Analyst";
+            personToTest.Description = "YOLO";
+            personToTest.Password = "123456";
+
+            PersonDAO personDAO = new PersonDAO();
+
+            personDAO.Insert(context, personToTest);
         }
 
         [TestMethod]
