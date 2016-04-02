9cc9f0b1f14ea6f8c6e30d600b2047b376424624
laurentboisvert <laurent.boisvert.1@collegeahuntsic.qc.ca>
2016-04-02 10:57:30 -0400

    Started Group page

---
+++ Pigeon_website/Group.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.master" AutoEventWireup="true" CodeFile="Group.aspx.cs" Inherits="Group" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <div class="container">
+
+        <div class="row">
+
+            <asp:Label runat="server" ID="lblTest"></asp:Label>
+            <asp:Button ID="BtnTest" runat="server" Text="Button" OnClick="BtnTest_Click" />
+        </div>
+
+    </div>
+
+
+</asp:Content>
+
---
+++ Pigeon_website/Group.aspx.cs
@@ -0,0 +1,73 @@
+﻿using PigeonsLibrairy.Facade.Implementation;
+using PigeonsLibrairy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Group : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            lblTest.Text = "Ceci n'est pas un postback";
+        } else
+        {
+            lblTest.Text = "Ceci est un postback";
+        }
+
+        // Also check if the group actually exists and if the user is following it
+        if(Session["user"] == null || Request.Params["groupID"] == null)
+        {
+            Response.Redirect("Index.aspx");
+        } else
+        {
+            person currentUser = (person) Session["user"];
+
+            // Get group ID from url parameter
+            Boolean goodGroupId = false;
+            int groupId;
+
+            goodGroupId = int.TryParse(Request.Params["groupID"], out groupId);
+
+            if(goodGroupId)
+            {
+                // Render group to page
+                lblTest.Text = "You want to see group: " + groupId;
+                renderGroupToPage(groupId);
+            } else
+            {
+                lblTest.Text = "Not a valid group/group not found";
+            }
+        }
+    }
+
+    protected void renderGroupToPage(int groupId)
+    {
+
+        GroupFacade gf = new GroupFacade();
+
+        group theGroup;
+
+        // TODO: Faire une méthode bool groupExists(int groupId)
+        try
+        {
+            theGroup = gf.GetGroupByID(groupId);
+            lblTest.Text = theGroup.Creation_date + " --- " + theGroup.Description;
+
+        } catch(Exception e)
+        {
+            lblTest.Text = "Group not found: " + e.Message;
+            Console.WriteLine("Group not found: " + e.Message);
+        }
+        
+    }
+
+    protected void BtnTest_Click(object sender, EventArgs e)
+    {
+        // fun
+    }
+}
