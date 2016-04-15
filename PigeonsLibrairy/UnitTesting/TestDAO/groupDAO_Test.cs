449fab562ab3796484ce9d72fb80a79f0c972052
kezfran <kezfranz@yahoo.fr>
2016-04-15 12:09:10 -0400

    modif final sur groupDAO_Test

--- PigeonsLibrairy/UnitTesting/TestDAO/groupDAO_Test.cs
+++ PigeonsLibrairy/UnitTesting/TestDAO/groupDAO_Test.cs
@@ -2,6 +2,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PigeonsLibrairy.Model;
 using PigeonsLibrairy.DAO.Implementation;
+using UnitTesting.TestDAO;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
 
 namespace UnitTesting.TestDAO
 {
@@ -30,75 +34,123 @@ namespace UnitTesting.TestDAO
             groupTest.Name = GROUP_NAME;
             groupTest.Creation_date = GROUP_CREATION_DATE;
             groupTest.Description = GROUP_DESCRIPTION;
-            groupTest.pi
+            groupTest.Group_picture_link = GROUP_PICTURE_LINK;
+            groupTest.Is_active = GROUP_IS_ACTIVE;
         }
 
+        /// <summary>
+        /// Remet le DAO et le group à null
+        /// </summary>
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            groupDAO = null;
+            groupTest = null;
+        }
 
+        /// <summary>
+        /// Test pour la méthode Insert de la classes <see cref="PigeonsLibrairy.DAO.Implementation.GroupDAO"/>
+        /// Insertion d'un Group et validation de ses propriétés
+        /// </summary>
         [TestMethod]
-        public void InsertGroupTest()
+        public void TestInsertGroup()
         {
             using (var context = new pigeonsEntities1())
             {
-                group groupToTest = new group();
-
-                groupToTest.Name = GROUP_NAME;
-                groupToTest.Creation_date = DateTime.Now;
-                groupToTest.Description = "WE ROCK";
-                groupToTest.Group_picture_link = "www.flickr.com";
-                groupToTest.Is_active = true;
-
-                GroupDAO groupDAO = new GroupDAO();
-
-                groupDAO.Insert(context, groupToTest);
+                groupDAO.Insert(context, groupTest);
                 context.SaveChanges();
-                GROUP_ID = groupToTest.Id;
+                int insertedGroupID = groupTest.Id;
 
-                Assert.AreEqual(groupToTest.Id, GROUP_ID);
+                group actualGroup = groupDAO.GetByID(context, insertedGroupID);
+                
+                Assert.AreEqual(GROUP_NAME, actualGroup.Name);
+                Assert.AreEqual(GROUP_CREATION_DATE, actualGroup.Creation_date);
+                Assert.AreEqual(GROUP_DESCRIPTION, actualGroup.Description);
+                Assert.AreEqual(GROUP_PICTURE_LINK, actualGroup.Group_picture_link);
+                Assert.AreEqual(GROUP_IS_ACTIVE, actualGroup.Is_active);
+
+                groupDAO.Delete(context, insertedGroupID);
+                context.SaveChanges();
             }
         }
 
+        /// <summary>
+        /// Test pour la méthode Delete de la classes <see cref="PigeonsLibrairy.DAO.Implementation.GroupDAO"/>
+        /// Insertion d'un Group et validation de ses propriétés
+        /// </summary>
         [TestMethod]
-        public void GetByIdGroupTest()
+        public void TestDeleteGroup()
         {
             using (var context = new pigeonsEntities1())
             {
-                GroupDAO groupDAO = new GroupDAO();
+                groupDAO.Insert(context, groupTest);
+                context.SaveChanges();
+                int insertedGroupID = groupTest.Id;
 
-                group groupCheck = groupDAO.GetByID(context, GROUP_ID);
+                group actualGroup = groupDAO.GetByID(context, insertedGroupID);
+                Assert.AreEqual(groupTest, actualGroup);
 
-                Assert.AreEqual(groupCheck.Name, GROUP_NAME);
+                groupDAO.Delete(context, insertedGroupID);
+                context.SaveChanges();
+
+                group DeletedGroup = groupDAO.GetByID(context, insertedGroupID);
+                Assert.AreEqual(null, DeletedGroup);
             }
         }
 
+        /// <summary>
+        /// Test pour la méthode Update de la classes <see cref="PigeonsLibrairy.DAO.Implementation.GroupDAO"/>
+        /// Insertion d'un Group et validation de ses propriétés
+        /// </summary>
         [TestMethod]
-        public void UpdateGroupTest()
+        public void TestUpdateGroup()
         {
+            const string expected_UpdatedName = "We've got to change the name";
+
             using (var context = new pigeonsEntities1())
             {
-                GroupDAO groupDAO = new GroupDAO();
-                group groupToTest = groupDAO.GetByID(context, GROUP_ID);
+                groupDAO.Insert(context, groupTest);
+                context.SaveChanges();
+                int insertedGroupID = groupTest.Id;
 
-                String modif = "TesaTesa";
-                groupToTest.Description = modif;
+                group actualGroup = groupDAO.GetByID(context, insertedGroupID);
+                actualGroup.Name = expected_UpdatedName;
 
-                groupDAO.Update(context, groupToTest);
+                groupDAO.Update(context, actualGroup);
                 context.SaveChanges();
 
-                Assert.AreEqual(groupToTest.Description, modif);
+                group updatedGroup = groupDAO.GetByID(context, insertedGroupID);
+                Assert.AreEqual(expected_UpdatedName, updatedGroup.Name);
+
+                groupDAO.Delete(context, insertedGroupID);
+                context.SaveChanges();
             }
         }
 
+        /// <summary>
+        /// Test pour la méthode GetBy column name de la classes <see cref="PigeonsLibrairy.DAO.Implementation.GroupDAO"/>
+        /// Insertion d'une Person et validation de ses propriétés
+        /// </summary>
         [TestMethod]
-        public void DeleteGroupTest()
+        public void TestGetByPerson()
         {
             using (var context = new pigeonsEntities1())
             {
-                GroupDAO groupDAO = new GroupDAO();
-
-                groupDAO.Delete(context, GROUP_ID);
+                groupDAO.Insert(context, groupTest);
                 context.SaveChanges();
+                int insertedGroupID = groupTest.Id;
 
-                Assert.AreEqual(null, GROUP_ID);
+                List<group> groupByName = groupDAO.GetBy(context, group.COLUMN_NAME, GROUP_NAME).ToList();
+                Assert.AreEqual(GROUP_NAME, groupByName[0].Name);
+
+                List<group> groupByDescription = groupDAO.GetBy(context, group.COLUMN_DESCRIPTION, GROUP_DESCRIPTION).ToList();
+                Assert.AreEqual(GROUP_DESCRIPTION, groupByDescription[0].Description);
+
+                List<group> groupByCreationDate = groupDAO.GetBy(context, group.COLUMN_CREATION_DATE, GROUP_CREATION_DATE).ToList();
+                Assert.AreEqual(GROUP_CREATION_DATE, groupByCreationDate[0].Creation_date);
+
+                groupDAO.Delete(context, insertedGroupID);
+                context.SaveChanges();
             }
         }
     }
